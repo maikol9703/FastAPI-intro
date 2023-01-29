6b219f601e2e46d2a07554c6b4433d3bcbe42b91
maikol9703 <maikolcristhian1997@gmail.com>
2023-01-28 23:22:27 -0600

    Look for user's info through their id

--- users.py
+++ users.py
@@ -22,5 +22,10 @@ async def usersjson():
 async def users():
     return users_list
 
-
-
+@app.get("/user/{id}")
+async def user(id:int):
+    users= filter(lambda user:user.id==id,users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error":"no se ha encontrado el usuario"}
