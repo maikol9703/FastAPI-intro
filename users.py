a3730cdfcdd7b6d82cd5955546e008085de32e36
maikol9703 <maikolcristhian1997@gmail.com>
2023-01-29 19:34:15 -0600

    Use post to create new users
    This will check if our new user already exists or not, and if it does
    give an error message

--- users.py
+++ users.py
@@ -22,7 +22,7 @@ async def usersjson():
 async def users():
     return users_list
 
-#Path search
+#Path search, demands an id
 
 @app.get("/user/{id}")
 async def user(id:int):
@@ -30,7 +30,7 @@ async def user(id:int):
     try:
         return list(users)[0]
     except:
-        return {"error":"no se ha encontrado el usuario"}
+        return {"error":"user not found"}
 
 #Query search, look up something adding /?<example>=<value>
 
@@ -38,9 +38,16 @@ async def user(id:int):
 async def userquery(id:int):
     return searchuser_id(id)
 
+@app.post("/user/")
+async def user(user:User):
+    if type(searchuser_id(user.id))==User:
+         return {"error":"user already registered"}
+    else:
+        users_list.append(user)
+
 def searchuser_id(id:int):
     users= filter(lambda user:user.id==id,users_list)
     try:
         return list(users)[0]
     except:
-        return {"error":"no se ha encontrado el usuario"}
+        return {"error":"user not found"}
