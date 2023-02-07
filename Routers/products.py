8945199a50bfa6af28832b3582ee8e0b3eb28ecf
maikol9703 <maikolcristhian1997@gmail.com>
2023-02-06 22:04:34 -0600

    Add prefix to save time when creating a root

--- Routers/products.py
+++ Routers/products.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
 
-router=APIRouter()
+router=APIRouter(prefix="/products",tags=["products"],responses={404:{"message":"page not found"}})
 
-@router.get("/products/")
+products_list=["banana","iphone","rice","ice cream"]
+
+@router.get("/")
 async def products():
-    return ["banana","manzana","iphone"]
+    return products_list
+
+@router.get("/{id}")
+async def products(id:int):
+    return products_list[id]
