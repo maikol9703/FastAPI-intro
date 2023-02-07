5c8ff50525dfc2651c319548444f66e381cf705d
maikol9703 <maikolcristhian1997@gmail.com>
2023-02-06 21:08:44 -0600

    Edit main.py to add products and users instances
    This makes possible access products and users from main
    server when this is running and no start each other apart

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+#pip install "fastapi[all]"
+
 from fastapi import FastAPI
+from Routers import products
+from Routers import users
 
 app=FastAPI()
+
+#Router
+app.include_router(products.router)
+app.include_router(users.router)
 
 @app.get('/')
 async def root():
