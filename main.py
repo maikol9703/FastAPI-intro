6b0bc63b2211aaac393d4414d850a89d9b28f545
maikol9703 <maikolcristhian1997@gmail.com>
2023-02-07 20:21:34 -0600

    Add displaying static files feature
    Import staticfiles instance to display static files in our server

--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 from fastapi import FastAPI
 from Routers import products
 from Routers import users
+from fastapi.staticfiles import StaticFiles
 
 app=FastAPI()
 
 #Router
 app.include_router(products.router)
 app.include_router(users.router)
+app.mount("/static",StaticFiles(directory="static"),name="static")
 
 @app.get('/')
 async def root():
